7a4ef869610826b6d42fef9d33a432d0e7f83688
fufako <patryko.mikiciuk@gmail.com>
2022-08-15 11:18:35 +0200

    Add validateInput func

--- src/getData.js
+++ src/getData.js
@@ -2,6 +2,7 @@ import { format } from "date-fns"
 
 export async function getData() {
   const userInput = document.querySelector("#users-Input")
+  validateInput(userInput)
   const response = await fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${userInput.value}&APPID=b36c6d2968b56b6cac14c9e3a395fb53&units=metric`,
     { mode: "cors" }
@@ -11,4 +12,14 @@ export async function getData() {
 }
 export function getTodayDate() {
   return format(new Date(), "EEEE, MMMM do")
+}
+function validateInput(userInput) {
+  const validateInfo = document.querySelector(".validate-info")
+  if (!/^[a-zA-Z]*$/.test(userInput.value) || userInput.value === "") {
+    validateInfo.style.display = "block"
+    validateInfo.innerHTML = `Location not found.<br>
+    Search must be in the form of "City", "City, State" or "City, Country".`
+  } else {
+    validateInfo.style.display = "none"
+  }
 }
